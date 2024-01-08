14e3230a3f0906abd9671e59a8c74182f91c3fd9
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2024-01-08 18:10:35 +0430

    setup deployment for render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-
+   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
